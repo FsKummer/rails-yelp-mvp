3cabe191b30df730c54c19df846e0c08e4d0b947
Felipe Kummer <felipe.s.kummer@gmail.com>
2022-11-16 14:53:15 -0300

    first version of Yelp MVP completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+puts 'Starting seed'
+10.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name, category: Restaurant::CATEGORIES.sample, address: Faker::Address.street_address,
+                    phone_number: Faker::PhoneNumber.cell_phone)
+  puts 'restaurant created'
+  5.times do
+    Review.create(content: Faker::Restaurant.review, rating: rand(0..5), restaurant:)
+    puts 'review created'
+  end
+end
+puts 'Seeds done :)'
